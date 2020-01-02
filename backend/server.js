e307e92e559d5c9a1006eedc710d331f8dc765f6
NonsoAmadi10 <nonsoamadi@aol.com>
2020-01-02 13:47:01 +0100

    ft-boilerplate: fix error

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('build'));
 
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve('../build/index.html'));
   });
 }
